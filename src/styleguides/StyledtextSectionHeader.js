5d199fe7b65e5f600768febb71b5fe7003d83255
mikkelgud <mikkel_gudmundsen@hotmail.com>
2019-08-30 17:13:50 +0200

    did facking nothing, just program fail with no reason

--- src/styleguides/StyledtextSectionHeader.js
+++ src/styleguides/StyledtextSectionHeader.js
@@ -1,6 +1,6 @@
-import Styled from 'styled-components';
+import styled from 'styled-components';
 
-export const StyledTextSectionHeader = Styled.h2`
+export const StyledTextSectionHeader = styled.h2`
 font-family: 'Oswald', sans-serif;
 font-weight: lighter; 
 font-size: 3rem;
