7595b986bd27723d078affbc8418b1073690c2e0
mikkelgud <mikkel_gudmundsen@hotmail.com>
2019-09-03 01:10:48 +0200

    deleted emoji :(

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ justify-content: center;
 margin-left: 2rem;
 `
 
-const aboutUsEmoji= "👨🏽‍💻👩🏻‍💻👨🏼‍💻";
+// const aboutUsEmoji= "👨🏽‍💻👩🏻‍💻👨🏼‍💻";
 
 //travels
 const travelHeader = "Route";
@@ -38,7 +38,8 @@ const leadersIcon = "users";
 const leadersText = "Read all about the ITTC-team´s coaches and leaders. Get to know the people who will make you climb the levels in table tennis, aswell as taking care of you.";
 const leadersTo = "/leaders";
 //about us
-const aboutUsHeader = "About Us "+aboutUsEmoji;
+const aboutUsHeader = "About Us ";
+const aboutUsIcon = "users-cog";
 const aboutUsText = "Read about ITTC, our goals and motivation for holding the camps. Here you can also learn more about the camps, and see what we are all about!";
 const aboutUsTo = "/aboutus";
 //sign up
@@ -49,32 +50,36 @@ const signUpTo = "/Signup";
 
 
  const informationLinks= [
- {
+    {
+        header: aboutUsHeader,
+        text: aboutUsText,
+        to: aboutUsTo,
+        icon: aboutUsIcon
+    },
+    {
+        header: stayHeader,
+        text: stayText,
+        to: stayTo,
+        icon: stayIcon
+    },
+    {
+        header: leadersHeader,
+        text: leadersText,
+        to: leadersTo,
+        icon: leadersIcon
+    },
+    {
     header: travelHeader,
     text: travelText,
     to: travelTo,
     icon: travelIcon
 },
-{
-    header: aboutUsHeader,
-    text: aboutUsText,
-    to: aboutUsTo
-}, {
+ {
     header: safteyHeader,
     text: safteyText,
     to: safteyTo,
     icon: safteyIcon
-}, {
-    header: stayHeader,
-    text: stayText,
-    to: stayTo,
-    icon: stayIcon
-}, {
-    header: leadersHeader,
-    text: leadersText,
-    to: leadersTo,
-    icon: leadersIcon
-}, {
+},   {
     header: signUpHeader,
     text: signUpText,
     to: signUpTo,
