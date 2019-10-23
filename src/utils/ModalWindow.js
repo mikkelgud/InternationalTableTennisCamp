87924bc47a3a9c91752b9e869fc265f223a817be
mikkelgud <mikkel_gudmundsen@hotmail.com>
2019-10-24 01:27:42 +0200

    Little bugFIX

--- src/utils/ModalWindow.js
+++ src/utils/ModalWindow.js
@@ -13,26 +13,36 @@ cursor: pointer;
 
 @media (max-width: 800px){
   border-radius: 0;
+  height: 100vw;
+
 }
 `;
 
 const StyledIcon = styled.h1`
 font-size: 30px;
-    cursor: pointer;
-    position: fixed;
-    z-index: 3;
-    margin: 14px 0 0 14px;
+cursor: pointer;
+position: fixed;
+text-align: right;
+color: #6b6b6b;
+z-index: 3;
+margin: 14px 0 0 14px;
    &:hover {
-     color: darkblue;
+     color: white;
+    }
+    @media (max-width: 800px){
+      border-radius: 0;
+      height: 100vw;
+      color: white;
+      
     }
 `;
 
 const ModalWindow = (props) => {
   return (
-    <> <StyledIcon onClick={props.onClick}>
+    <>
+     <StyledIcon onClick={props.onClick}>
        <FontAwesomeIcon icon="times"/>  </StyledIcon>
     <FullScreenPicture src={props.source} display={props.display} onClick={props.onClick}/>
-
     </>
     );
 }
