d020ba67ceb01f2d0421f6a9dfb5ab8b91caef17
mikkelgud <mikkel_gudmundsen@hotmail.com>
2019-11-29 23:19:09 +0100

    Added better styling for faq

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -7,10 +7,12 @@ import { ScrollTop } from '../utils/ScrollToTopp';
 import { StyledSTextSectionHeader } from '../styleguides/StyledSemiTextSectionHeader';
 
 
-const StyledDetils = styled.details`
-border: 1px solid black;
-padding: 2rem;
+const StyledDetails = styled.details`
+padding: 0;
 border-radius: 15px;
+border: 1px solid black;
+padding: 0.5rem;
+box-shadow: 0 1px 6px 0 rgba(32,33,36,0.28);
 `;
 
 export default function Rules() {
@@ -21,21 +23,23 @@ export default function Rules() {
             <StyledTextSectionHeader style={{textAlign: "center"}}>Frequently Asked Questions</StyledTextSectionHeader>
             <br/>
             <br/>
-            <StyledText>
-                <StyledDetils>
-                    <summary>Is the sign up binding? </summary>
-                    <div>
-                    <br/>
-                         <StyledSTextSectionHeader>For camp Taiwan:</StyledSTextSectionHeader>
-                        No, but you will not get any refound on your flight ticket.
+                <StyledDetails>
+               <summary> <StyledSTextSectionHeader>Is the sign up binding? </StyledSTextSectionHeader></summary>
+                    <div style={{borderTop: "1px solid #e1e4e8", paddingTop: "1rem"}}>
+                    
+                         <StyledSTextSectionHeader style={{padding: "0 2rem"}}>For camp Taiwan:</StyledSTextSectionHeader>
+                         <StyledText style={{padding: "0 2rem"}}>  No, but you will not get any refound on your flight ticket.
                         If you fix the travel yourself, there's a deposit of 2000 NOK.
                        You pay the ticket or deposit after we have confirmed your sign up and sent you an email back on that.
+                       </StyledText>
                         <br/><br/>
-                        <StyledSTextSectionHeader>For camp Norway:</StyledSTextSectionHeader>
-                        No. Full refound.
+                        <StyledSTextSectionHeader style={{padding: "0 2rem"}}>For camp Norway:</StyledSTextSectionHeader>
+                       <StyledText style={{padding: "0 2rem"}}> No binding. Full refound if you can't come.</StyledText>
+                    <br/>
                     </div>
-                </StyledDetils>
-            </StyledText>
+               
+                </StyledDetails>
+  
             <br/>
             <br/>
         </StyledDiv>
