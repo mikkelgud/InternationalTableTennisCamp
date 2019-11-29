d48c95fe3dde9e6199e3fb360f2d41d0cb627c96
mikkelgud <mikkel_gudmundsen@hotmail.com>
2019-11-29 17:42:26 +0100

    changed header tags to h1 and h2 for google search

--- src/styleguides/StyledSectionHeader.js
+++ src/styleguides/StyledSectionHeader.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const StyledTextSectionHeader = styled.header`
+export const StyledTextSectionHeader = styled.h1`
 font-family: 'Oswald', sans-serif;
 font-weight: lighter; 
 font-size: 2.4rem;
--- src/styleguides/Styledh3.js
+++ src/styleguides/Styledh3.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const StyledH3 = styled.h3`
+export const StyledH3 = styled.h2`
 font-weight: bold;
 font-size: 1.2rem;
 font-family: 'roboto', sans-serif;
