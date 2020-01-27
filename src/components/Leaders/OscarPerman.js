5d45a0e3c005025e2151ed5d5c9743b92911c216
mikkelgud <mikkel_gudmundsen@hotmail.com>
2020-01-27 15:23:17 +0100

    Deleted pictiure of Oscar

--- src/components/Leaders/OscarPerman.js
+++ src/components/Leaders/OscarPerman.js
@@ -3,10 +3,8 @@ import { StyledTextSectionHeader } from '../../styleguides/StyledSectionHeader';
 import { StyledText } from '../../styleguides/StyledText';
 import StyledTextBoxDiv from '../../styleguides/StyledTextBoxDiv';
 import {ScrollTop}  from '../../utils/ScrollToTopp';
-import OscarPermanPicture from '../../Assets/OscarPermanPicture.jpeg';
 import { InfoBox } from '../../styleguides/StyledInfoBox';
 import { StyledH3 } from '../../styleguides/Styledh3';
-import { StyledImageForLeaders } from '../../styleguides/StyledImageForLeaders';
 
 
 export default function Oscar() {
@@ -16,9 +14,9 @@ export default function Oscar() {
       <StyledTextSectionHeader>
        Oscar Perman
        </StyledTextSectionHeader>
-       <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+       {/* <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
           <StyledImageForLeaders src={OscarPermanPicture}  />
-        </div>
+        </div> */}
       <StyledText>
           Oscar is headcoach in B-75 on daily basis. He is a young hard working coach, 
           and has been a great player aswell. He has been traveling all around the world playing table tennis. 
